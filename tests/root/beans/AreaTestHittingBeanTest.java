398ef68cae98818da4dd457af43ec72aa9c707ce
come_ill_foo <rayveleaveme34@inbox.ru>
2021-04-21 03:27:13 +0300

    feat: connected with resource bundles for localization

--- tests/root/beans/AreaTestHittingBeanTest.java
+++ tests/root/beans/AreaTestHittingBeanTest.java
@@ -3,59 +3,73 @@ package root.beans;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class AreaTestHittingBeanTest {
   private static final double EQUAL_DELTA = Double.MIN_VALUE;
+  private static final ResourceBundle testsBundle = ResourceBundle.getBundle( "root.beans.locals.native.russian.areaTestHittingBeanTests", Locale.getDefault( ) );
   @Test
   public void testConstructorRadiusSet( ) {
+    final String msg = testsBundle.getString( "testConstructorRadiusSet" );
     double radius = 8;
-    Assert.assertEquals( "Некорректная установка поля radius в конструкторе", radius, new AreaTestHittingBean( radius ).RADIUS, EQUAL_DELTA );
+    Assert.assertEquals( msg, radius, new AreaTestHittingBean( radius ).RADIUS, EQUAL_DELTA );
   }
 
   @Test
   public void testDefaultErrorValue( ) {
-    Assert.assertEquals( "Неправильное значение по умолчанию поля error", AreaTestHittingBean.DEFAULT_ERROR, new AreaTestHittingBean( 1 ).getError(), EQUAL_DELTA );
+    final String msg = testsBundle.getString( "testDefaultErrorValue" );
+    Assert.assertEquals( msg, AreaTestHittingBean.DEFAULT_ERROR, new AreaTestHittingBean( 1 ).getError(), EQUAL_DELTA );
   }
 
   @Test
   public void testErrorSetter( ) {
+    final String msg = testsBundle.getString( "testErrorSetter" );
     double error = 8;
     AreaTestHittingBean a = new AreaTestHittingBean( 1 );
     a.setError( error );
-    Assert.assertEquals( "Неправильная работа аксессора set поля error", error, a.getError(), EQUAL_DELTA );
+    Assert.assertEquals( msg, error, a.getError(), EQUAL_DELTA );
   }
 
   @Test
   public void testHitInEmptySpaceFalse( ) {
-    Assert.assertFalse( "Неправильное вычисление попадания в пустую область", new AreaTestHittingBean( 3 ).test( 2, 2 ) );
+    final String msg = testsBundle.getString( "testHitInEmptySpaceFalse" );
+    Assert.assertFalse( msg, new AreaTestHittingBean( 3 ).test( 2, 2 ) );
   }
 
   @Test
   public void testHitInQuadrantTrue( ) {
-    Assert.assertTrue( "Неправильное вычисление попадания в область кругового сектора", new AreaTestHittingBean( 3 ).test( -2, 2 ) );
+    final String msg = testsBundle.getString( "testHitInQuadrantTrue" );
+    Assert.assertTrue( msg, new AreaTestHittingBean( 3 ).test( -2, 2 ) );
   }
 
   @Test
   public void testHitInQuadrantFalse( ) {
-    Assert.assertFalse( "Неправильное вычисление попадания в область кругового сектора", new AreaTestHittingBean( 3 ).test( -3, 3 ) );
+    final String msg = testsBundle.getString( "testHitInQuadrantFalse" );
+    Assert.assertFalse( msg, new AreaTestHittingBean( 3 ).test( -3, 3 ) );
   }
 
   @Test
   public void testHitInRectangleTrue( ) {
-    Assert.assertTrue( "Неправильное вычисление попадания в область прямоугольника", new AreaTestHittingBean( 3 ).test( -1, -1 ) );
+    final String msg = testsBundle.getString( "testHitInRectangleTrue" );
+    Assert.assertTrue( msg, new AreaTestHittingBean( 3 ).test( -1, -1 ) );
   }
 
   @Test
   public void testHitInRectangleFalse( ) {
-    Assert.assertFalse( "Неправильное вычисление попадания в область прямоугольника", new AreaTestHittingBean( 3 ).test( -2, -1 ) );
+    final String msg = testsBundle.getString( "testHitInRectangleFalse" );
+    Assert.assertFalse( msg, new AreaTestHittingBean( 3 ).test( -2, -1 ) );
   }
 
   @Test
   public void testHitInTriangleTrue( ) {
-    Assert.assertTrue( "Неправильное вычисление попадания в область треугольника", new AreaTestHittingBean( 3 ).test( 1.5, -1.5 ) );
+    final String msg = testsBundle.getString( "testHitInTriangleTrue" );
+    Assert.assertTrue( msg, new AreaTestHittingBean( 3 ).test( 1.5, -1.5 ) );
   }
 
   @Test
   public void testHitInTriangleFalse( ) {
-    Assert.assertFalse( "Неправильное вычисление попадания в область треугольника", new AreaTestHittingBean( 3 ).test( 2, -1.5 ) );
+    final String msg = testsBundle.getString( "testHitInTriangleFalse" );
+    Assert.assertFalse( msg, new AreaTestHittingBean( 3 ).test( 2, -1.5 ) );
   }
 }
--- tests/root/beans/ResultBeanTest.java
+++ tests/root/beans/ResultBeanTest.java
@@ -4,74 +4,89 @@ import org.junit.Assert;
 import org.junit.Test;
 import root.beans.ResultBean;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ResultBeanTest {
   private static final ResultBean FIRST = new ResultBean( -1, 2, 3, true );
   private static final ResultBean SECOND_EQUALS_TO_FIRST = new ResultBean( -1, 2, 3, true );
   private static final ResultBean SECOND_NOT_EQUALS_TO_FIRST = new ResultBean( -1, 2, 3, false );
   private static final double EQUAL_DELTA = Double.MIN_VALUE;
+  private static final ResourceBundle testsBundle = ResourceBundle.getBundle("root.beans.locals.native.russian.resultBeanTests", Locale.getDefault( ) );
 
   @Test
   public void testXGetter( ) {
-    Assert.assertEquals( "Значения поля X и его аксессора не совпадают", FIRST.X, FIRST.getX(), EQUAL_DELTA );
+    final String message = testsBundle.getString( "testXGetter" );
+    Assert.assertEquals( message, FIRST.X, FIRST.getX(), EQUAL_DELTA );
   }
 
   @Test
   public void testYGetter() {
-    Assert.assertEquals( "Значения поля Y и его аксессора не совпадают", FIRST.Y, FIRST.getY(), EQUAL_DELTA );
+    final String message = testsBundle.getString( "testYGetter" );
+    Assert.assertEquals( message, FIRST.Y, FIRST.getY(), EQUAL_DELTA );
   }
 
   @Test
   public void testRGetter() {
-    Assert.assertEquals( "Значения поля R и его аксессора не совпадают", FIRST.R, FIRST.getR(), EQUAL_DELTA );
+    final String message = testsBundle.getString( "testRGetter" );
+    Assert.assertEquals( message, FIRST.R, FIRST.getR(), EQUAL_DELTA );
   }
 
   @Test
   public void testHitGetter() {
-    Assert.assertEquals( "Значения поля HIT и его аксессора не совпадают", FIRST.HIT, FIRST.isHit() );
+    final String message = testsBundle.getString( "testHitGetter" );
+    Assert.assertEquals( message, FIRST.HIT, FIRST.isHit() );
   }
 
   @Test
   public void testEqualsWithSameResultsParameters( ) {
-    Assert.assertEquals( String.format( "Результаты [ %s ] с одинаковыми параметрами не равны",
+    final String message = testsBundle.getString( "testEqualsWithSameResultsParameters" );
+    Assert.assertEquals( String.format( message,
         FIRST ), FIRST, SECOND_EQUALS_TO_FIRST );
   }
 
   @Test
   public void testEqualsWithSameResult( ) {
-    Assert.assertEquals( String.format( "Результат [ %s ] не равен самому себе", FIRST ), FIRST, FIRST );
+    final String message = testsBundle.getString( "testEqualsWithSameResult" );
+    Assert.assertEquals( String.format( message, FIRST ), FIRST, FIRST );
   }
 
   @Test
   public void testEqualsWithDifferentResults( ) {
-    Assert.assertNotEquals( String.format( "Результаты [ %s ] и [ %s ] равны, несмотря на разные параметры",
+    final String message = testsBundle.getString( "testEqualsWithDifferentResults" );
+    Assert.assertNotEquals( String.format( message,
         FIRST, SECOND_NOT_EQUALS_TO_FIRST ), FIRST, SECOND_NOT_EQUALS_TO_FIRST );
   }
 
   @Test
   public void testEqualsCommutativityProperty( ) {
-    Assert.assertEquals( "Функция equals бина не обладает свойством коммутативности",
+    final String message = testsBundle.getString( "testEqualsCommutativityProperty" );
+    Assert.assertEquals( message,
         FIRST.equals( SECOND_NOT_EQUALS_TO_FIRST ),
         SECOND_NOT_EQUALS_TO_FIRST.equals( FIRST ) );
   }
 
   @Test
   public void testEqualsWithNaN( ) {
+    final String message = testsBundle.getString( "testEqualsWithNaN" );
     ResultBean NaNFirstBean = new ResultBean( Double.NaN, 0, 0, false );
     ResultBean NaNSecondBean = new ResultBean( Double.NaN, 0, 0, false );
-    Assert.assertEquals( "Некорректное сравнение с NaN", NaNFirstBean, NaNSecondBean );
+    Assert.assertEquals( message, NaNFirstBean, NaNSecondBean );
   }
 
   @Test
   public void testEqualsWithPositiveInfinite( ) {
+    final String message = testsBundle.getString( "testEqualsWithPositiveInfinite" );
     ResultBean NaNFirstBean = new ResultBean( Double.POSITIVE_INFINITY, 0, 0, false );
     ResultBean NaNSecondBean = new ResultBean( Double.POSITIVE_INFINITY, 0, 0, false );
-    Assert.assertEquals( "Некорректное сравнение с положительной бесконечностью", NaNFirstBean, NaNSecondBean );
+    Assert.assertEquals( message, NaNFirstBean, NaNSecondBean );
   }
 
   @Test
   public void testEqualsWithNegativeInfinite( ) {
+    final String message = testsBundle.getString( "testEqualsWithNegativeInfinite" );
     ResultBean NaNFirstBean = new ResultBean( Double.NEGATIVE_INFINITY, 0, 0, false );
     ResultBean NaNSecondBean = new ResultBean( Double.NEGATIVE_INFINITY, 0, 0, false );
-    Assert.assertEquals( "Некорректное сравнение с отрицательной бесконечностью", NaNFirstBean, NaNSecondBean );
+    Assert.assertEquals( message, NaNFirstBean, NaNSecondBean );
   }
 }
